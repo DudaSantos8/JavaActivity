633794032728b747ba8efd0a47da58b0eaaef9f1
Duda Santos <maria.oliveira@zup.com.br>
2024-07-17 10:54:12 -0300

    First commit

---
+++ src/SumNumbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class SumNumbers {
+    public static void main(String[] args) {
+
+    }
+    public static Scanner scanner(String message){
+        System.out.println(message);
+        return new Scanner(System.in);
+    }
+}
