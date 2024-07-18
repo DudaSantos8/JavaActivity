d58203d920ca266178e78a10b68238074cefcc72
Duda Santos <maria.oliveira@zup.com.br>
2024-07-18 20:45:10 -0300

    Add edition in SumNumbers

--- src/SumNumbers.java
+++ src/SumNumbers.java
@@ -2,7 +2,13 @@ import java.util.Scanner;
 
 public class SumNumbers {
     public static void main(String[] args) {
+        int number = scanner("Write a integer number: ").nextInt();
+        int calc = 0;
 
+        for (int i = 0; i <= number; i++) {
+            calc += i;
+        }
+        System.out.println("the sum of this number with its previous ones is: "+calc);
     }
     public static Scanner scanner(String message){
         System.out.println(message);
